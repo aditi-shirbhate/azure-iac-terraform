1a9a28c909e3dfec813a72ddb045540e644f3f1d
Aditi Shirbhate <70370853+aditi-shirbhate@users.noreply.github.com>
2026-06-23 15:42:03 +0530

    Create outputs.tf

---
+++ modules/aks/outputs.tf
@@ -0,0 +1,21 @@
+output "cluster_name" {
+  description = "Name of the AKS cluster"
+  value       = azurerm_kubernetes_cluster.aks.name
+}
+
+output "cluster_id" {
+  description = "ID of the AKS cluster"
+  value       = azurerm_kubernetes_cluster.aks.id
+}
+
+output "kube_config" {
+  description = "Kubeconfig for the AKS cluster"
+  value       = azurerm_kubernetes_cluster.aks.kube_config_raw
+  sensitive   = true
+}
+
+output "host" {
+  description = "AKS cluster host"
+  value       = azurerm_kubernetes_cluster.aks.kube_config[0].host
+  sensitive   = true
+}
