d4766599a7589fed56a6cd80ee34ec38eace4a59
Aditi Shirbhate <70370853+aditi-shirbhate@users.noreply.github.com>
2026-06-23 15:37:50 +0530

    Create variables.tf

---
+++ modules/network/variables.tf
@@ -0,0 +1,37 @@
+variable "resource_group_name" {
+  description = "Name of the resource group"
+  type        = string
+}
+
+variable "location" {
+  description = "Azure region"
+  type        = string
+  default     = "East US"
+}
+
+variable "vnet_name" {
+  description = "Name of the virtual network"
+  type        = string
+}
+
+variable "address_space" {
+  description = "Address space for the VNet"
+  type        = list(string)
+  default     = ["10.0.0.0/16"]
+}
+
+variable "subnet_name" {
+  description = "Name of the subnet"
+  type        = string
+}
+
+variable "subnet_prefixes" {
+  description = "Address prefixes for the subnet"
+  type        = list(string)
+  default     = ["10.0.1.0/24"]
+}
+
+variable "nsg_name" {
+  description = "Name of the network security group"
+  type        = string
+}
